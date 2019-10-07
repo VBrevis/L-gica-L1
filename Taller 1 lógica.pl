b0b998fcd132b5a86fb98daaca418b2be7707ada
Unknown <williams.alvarez@usach.cl>
2019-10-07 16:04:31 -0300

    Detección de defectos

--- Taller 1 lógica.pl
+++ Taller 1 lógica.pl
@@ -4,68 +4,54 @@
 % Nicolas Gutierrez Pereira
 % Michael Castillo Peñaloza
 % ----------------------------------------------------------------------
-%
+
+
 % ----------------------------------------------------------------------
 % Hechos para los defectos
-% defecto(a, b).
 
-defecto("Pedunculo", "Leve").
-defecto("Pedunculo", "Moderado").
-defecto("Pedunculo", "Severo").
-defecto("Pedunculo", "Muy severo").
-defecto("Piel", "Quemadura solar").
-defecto("Piel", "Pitting").
-
-dañoPredunculo(1, "Leve").
-dañoPredunculo(2, "Moderado").
-dañoPredunculo(3, "Severo").
-dañoPredunculo(4, "Muy severo").
-
-dañoPiel("Quemadura solar").
-dañoPiel("Pitting").
-dañoPiel("Machucon").
-dañoPiel("Magulladura").
-dañoPiel("Cicatriz").
-dañoPiel("Arrugado").
-
-partidura("Medialuna").
-partidura("Sutura").
-partidura("Apice").
-partidura("Lateral").
-
-anomalia("Fruto gemelo").
-anomalia("Fruto doble").
+defecto("Mancha", "Quemadura solar").
+defecto("Mancha", "Cicatriz").
+defecto("Patidura", "Medialuna").
+defecto("Partidura", "Sutura").
+defecto("Partidura", "Apice").
+defecto("Partidura", "Lateral").
+defecto("Anomalia", "Fruto gemelo").
+defecto("Anomalia", "Fruto doble").
+defecto("Anomalia", "Pitting").
+defecto("Anomalia", "Machucon").
+defecto("Anomalia", "Magulladura").
+defecto("Anomalia", "Arrugado").
 
 
 % ----------------------------------------------------------------------
-%
+
+
+
 % ----------------------------------------------------------------------
 % Hechos para caracterasticas del fruto
-color(1, "rosa").
-color(2, "rojo claro").
-color(3, "rojo oscuro").
-color(4, "rojo").
-color(5, "caoba").
-color(6, "caoba oscuro").
-color(7, "negro").
 
-firmeza(1, "No exportable").
-firmeza(2, "Exportable con riesgo").
-firmeza(3, "Exportable").
+caracteristica("Color", "Rosa").
+caracteristica("Color", "Rojo claro").
+caracteristica("Color", "Rojo").
+caracteristica("Color", "Rojo oscuro").
+caracteristica("Color", "Caoba").
+caracteristica("Color", "Caoba oscuro").
+caracteristica("Color", "Negro").
+caracteristica("Firmeza", "Extra blando").
+caracteristica("Firmeza", "Blando").
+caracteristica("Firmeza", "Rigido").
+caracteristica("Firmeza", "Muy rigido").
+caracteristica("Calibre", "Mediano").
+caracteristica("Calibre", "Largo").
+caracteristica("Calibre", "Extra largo").
+caracteristica("Calibre", "Jumbo").
+caracteristica("Calibre", "Gigante").
+caracteristica("Calibre", "Super gigante").
+caracteristica("Brillo", "Opaco").
+caracteristica("Brillo", "Brillante").
 
-calibre("M", "Mediano").
-calibre("L", "Largo").
-calibre("XL", "Extra largo").
-calibre("J", "Jumbo").
-calibre("G", "Gigante").
-calibre("SG", "Super gigante").
-
-brillo("Opaco").
-brillo("Brillante").
 
 
-caracteristicas(a, b, c).
-% ----------------------------------------------------------------------
 %
 % ----------------------------------------------------------------------
 % Hechos para la eleccion.
@@ -118,29 +104,25 @@ conjuntoDefectos(a, b, c):-
    defecto(a, b, c).
 % ----------------------------------------------------------------------
 
-metricaDañoPedunculo(Porcentaje, Tipo):-
-  Porcentaje =< 25,
-   N = 1,
-   dañoPredunculo(N, Tipo).
 
-metricaDañoPedunculo(Porcentaje, Tipo):-
-   Porcentaje > 25,
-   Porcentaje =< 50,
-   N = 2,
-   dañoPredunculo(N, Tipo).
+% consulta([caract_1, caract_2, ...], Lista_defectos).
 
-metricaDañoPedunculo(Porcentaje, Tipo):-
-   Porcentaje > 50,
-   Porcentaje =< 75,
-   N = 3,
-   dañoPredunculo(N, Tipo).
-
-metricaDañoPedunculo(Porcentaje, Tipo):-
-   Porcentaje > 75,
-   N = 4,
-   dañoPredunculo(N, Tipo).
-
-guinda(S, C1, C2):-defecto(_, C1), defecto(_,C2), S="No".
+consulta([], []).
+consulta([G|Gs], [G|Ss]):-
+   defecto(_, G), !,
+   consulta(Gs, Ss).
+consulta([G|Gs], [S|Ss]):-
+   G == "Negro",
+   S = "Madurez excesiva",
+   caracteristica("Color", G), !,
+   consulta(Gs, Ss).
+consulta([G|Gs], [S|Ss]):-
+   G == "Rosa",
+   S = "Sin color",
+   caracteristica("Color", G), !,
+   consulta(Gs, Ss).
+consulta([_|Gs],S):-
+   consulta(Gs, S).
 
 
 
