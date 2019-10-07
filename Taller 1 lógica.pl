877ef98242bf4ce91c2a9adb01151b95636496a3
Unknown <williams.alvarez@usach.cl>
2019-10-07 10:18:21 -0300

    Algunos hechos agregados

--- Taller 1 lógica.pl
+++ Taller 1 lógica.pl
@@ -1,22 +1,66 @@
 % Taller 1.
-% Williams Alvarez Gajardo 
-% Vicente Brevis MartÃ­nez 
-% NicolÃ¡s GutiÃ©rrez Pereira 
-% Michael Castillo PeÃ±aloza 
+% Williams Alvarez Gajardo
+% Vicente Brevis Martinez
+% Nicolas Gutierrez Pereira
+% Michael Castillo Peñaloza
 % ----------------------------------------------------------------------
 %
 % ----------------------------------------------------------------------
 % Hechos para los defectos
 defecto(a, b).
+
+dañoPredunculo(1, "Leve").
+dañoPredunculo(2, "Moderado").
+dañoPredunculo(3, "Severo").
+dañoPredunculo(4, "Muy severo").
+
+dañoPiel("Quemadura solar").
+dañoPiel("Pitting").
+dañoPiel("Machucon").
+dañoPiel("Magulladura").
+dañoPiel("Cicatriz").
+
+partidura("Medialuna").
+partidura("Sutura").
+partidura("Apice").
+partidura("Lateral").
+
+anomalia("Fruto gemelo").
+anomalia("Fruto doble").
+
+
 % ----------------------------------------------------------------------
 %
 % ----------------------------------------------------------------------
-% Hechos para caracterÃ­sticas del fruto
+% Hechos para caracterasticas del fruto
+color(1, "rosa").
+color(2, "rojo claro").
+color(3, "rojo oscuro").
+color(4, "rojo").
+color(5, "caoba").
+color(6, "caoba oscuro").
+color(7, "negro").
+
+firmeza(1, "No exportable").
+firmeza(2, "Exportable con riesgo").
+firmeza(3, "Exportable").
+
+calibre("M", "Mediano").
+calibre("L", "Largo").
+calibre("XL", "Extra largo").
+calibre("J", "Jumbo").
+calibre("G", "Gigante").
+calibre("SG", "Super gigante").
+
+brillo("Opaco").
+brillo("Brillante").
+
+
 caracteristicas(a, b, c).
 % ----------------------------------------------------------------------
 %
 % ----------------------------------------------------------------------
-% Hechos para la elecciÃ³n.
+% Hechos para la eleccion.
 eleccion(a, b, c).
 % ----------------------------------------------------------------------
 
@@ -31,7 +75,7 @@ perceptibles(a, b, c).
 % ----------------------------------------------------------------------
 %
 % ----------------------------------------------------------------------
-% Hechos salidas segÃºn calibre
+% Hechos salidas segun calibre
 salida(a, b, c).
 % ----------------------------------------------------------------------
 
@@ -46,7 +90,7 @@ salida(a, b, c).
 
 
 % ----------------------------------------------------------------------
-% FunciÃ³n que indica la salida en la lÃ­nea de embalaje
+% Funcion que indica la salida en la linea de embalaje
 %
 % Entrada:
 % Salida:
@@ -57,11 +101,22 @@ salida(a, b, c):-
 
 
 % ----------------------------------------------------------------------
-% Predicado que consulta dado un fruto entrege un conjunto de defectos si es que este posee 
+% Predicado que consulta dado un fruto entrege un conjunto de defectos si es que este posee
 % defectos establecidos...
 %
-% Entrada: 
-% Salida: 
+% Entrada:
+% Salida:
 conjuntoDefectos(a, b, c):-
    defecto(a, b, c).
 % ----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
